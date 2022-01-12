54c5d2104f38baf6bd1e1b9fbbc820a8b933491c
Andrew Furey <andrewfurey21@gmail.com>
2022-01-12 07:32:52 +0000

    finished cubies

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-const cubieLength = 0.1;
+const cubieLength = 0.2;
 const n = 3;
 
 let camera, scene, renderer;
 
 //Materials
 let white, yellow, green, blue, red, orange;
+let border;
 
 let cube;
 
@@ -21,38 +22,46 @@ function init() {
 
 	//Do stuff here
 	//Initializing materials
-	white = new THREE.MeshBasicMaterial({ color: 0xfff });
-	yellow = new THREE.MeshBasicMaterial({ color: 0xf7eb00 });
-	green = new THREE.MeshBasicMaterial({ color: 0x3cf02b });
-	blue = new THREE.MeshBasicMaterial({ color: 0x0004f2 });
-	red = new THREE.MeshBasicMaterial({ color: 0xc7170a });
-	orange = new THREE.MeshBasicMaterial({ color: 0xde852c });
+	white = new THREE.MeshBasicMaterial({
+		color: 0xffffff,
+		side: THREE.DoubleSide,
+	});
+	yellow = new THREE.MeshBasicMaterial({
+		color: 0xf7eb00,
+		side: THREE.DoubleSide,
+	});
+	green = new THREE.MeshBasicMaterial({
+		color: 0x3cf02b,
+		side: THREE.DoubleSide,
+	});
+	blue = new THREE.MeshBasicMaterial({
+		color: 0x0004f2,
+		side: THREE.DoubleSide,
+	});
+	red = new THREE.MeshBasicMaterial({
+		color: 0xc7170a,
+		side: THREE.DoubleSide,
+	});
+	orange = new THREE.MeshBasicMaterial({
+		color: 0xde852c,
+		side: THREE.DoubleSide,
+	});
 
-	let qb = new THREE.BoxGeometry(cubieLength, cubieLength, cubieLength);
+	border = new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 1 });
+
 	cube = new THREE.Group();
 
 	for (let i = -1; i < n - 1; i++) {
 		for (let j = -1; j < n - 1; j++) {
 			for (let k = -1; k < n - 1; k++) {
-				let cubie;
-				if ((i+1) % 2 == 1) {
-					if ((j+1) % 2 == 0) {
-						cubie = new THREE.Mesh(qb, (k+1) % 2 == 0 ? green : red);
-					} else {
-						cubie = new THREE.Mesh(qb, (k+1) % 2 == 0 ? red : green);
-					}
-				} else {
-					if ((j+1) % 2 == 0) {
-						cubie = new THREE.Mesh(qb, (k+1) % 2 == 0 ? red : green);
-					} else {
-						cubie = new THREE.Mesh(qb, (k+1) % 2 == 0 ? green : red);
-					}
-				}
+				let cubie = createCubie();
 				cubie.position.set(i * cubieLength, j * cubieLength, k * cubieLength);
 				cube.add(cubie);
 			}
 		}
 	}
+
+	// cube.add(createCubie())
 
 	scene.add(cube);
 	cube.rotation.x += 1;
@@ -67,14 +76,70 @@ function init() {
 function animation(time) {
 	//Apply all the changes to the scene and then render
 	cube.rotation.y += 0.02;
+	cube.rotation.z += 0.02;
 	renderer.render(scene, camera);
 }
 
 init();
 
+function createCubiePlane(
+	translateX,
+	translateY,
+	translateZ,
+	rotateX,
+	rotateY,
+	rotateZ,
+	color
+) {
+	let borderGeometry;
+	let wireframe;
+	let plane = new THREE.PlaneGeometry(cubieLength, cubieLength);
+	plane.rotateX(rotateX);
+	plane.rotateY(rotateY);
+	plane.rotateZ(rotateZ);
+	plane.translate(translateX, translateY, translateZ);
+
+	let planeMesh = new THREE.Mesh(plane, color);
+	borderGeometry = new THREE.EdgesGeometry(planeMesh.geometry);
+	wireframe = new THREE.LineSegments(borderGeometry, border);
+	wireframe.renderOrder = 1;
+	planeMesh.add(wireframe);
+	return planeMesh;
+}
+
 //returns a Group of planes that make up a cube with the correct colors
 function createCubie() {
 	let cubieMeshes = new THREE.Group();
-	//Add planes here
+
+	//front
+	cubieMeshes.add(
+		createCubiePlane(0, 0, cubieLength / 2, 0, 0, 0, green)
+	);
+
+	// back
+	cubieMeshes.add(
+		createCubiePlane(0, 0, -cubieLength / 2, 0, 0, 0, blue)
+		);
+
+	// //up
+	cubieMeshes.add(
+		createCubiePlane(0, cubieLength / 2, 0, Math.PI / 2, 0, 0, white)
+	);
+
+	// //down
+	cubieMeshes.add(
+		createCubiePlane(0, -cubieLength / 2, 0, Math.PI / 2, 0, 0, yellow)
+	);
+
+	//left
+	cubieMeshes.add(
+		createCubiePlane(cubieLength / 2, 0, 0, 0, Math.PI / 2, 0, orange)
+	);
+
+	//right
+	cubieMeshes.add(
+		createCubiePlane(-cubieLength / 2, 0, 0, 0, Math.PI / 2, 0, red)
+	);
+
 	return cubieMeshes;
 }
