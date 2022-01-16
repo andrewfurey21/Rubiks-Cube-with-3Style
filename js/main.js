d068b5efe0d30e54868a36d84d5d56b1a7b073ac
andrewfurey21 <andrewfurey2003@gmail.com>
2022-01-16 07:10:04 +0000

    got turning to work for L

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
-
-const n = 3	;
-const cubieLength = 0.4/n;
+const n = 3;
+const cubieLength = 0.4 / n;
 
 let cube;
 let camera, scene, renderer;
 
-
-//Turns (excluding anticlockwise) UDRLFBudrlfbMSE
-
 function init() {
-	//Initialie camera and scene
 	camera = new THREE.PerspectiveCamera(
 		70,
 		window.innerWidth / window.innerHeight,
@@ -21,9 +16,9 @@ function init() {
 
 	cube = new THREE.Group();
 
-	for (let i = -Math.floor(n/2); i < Math.ceil(n/2); i++) {
-		for (let j = -Math.floor(n/2); j < Math.ceil(n/2); j++) {
-			for (let k = -Math.floor(n/2); k < Math.ceil(n/2); k++) {
+	for (let i = -Math.floor(n / 2); i < Math.ceil(n / 2); i++) {
+		for (let j = -Math.floor(n / 2); j < Math.ceil(n / 2); j++) {
+			for (let k = -Math.floor(n / 2); k < Math.ceil(n / 2); k++) {
 				let cubie = new Cubie(i, j, k, cubieLength);
 				cube.add(cubie.mesh);
 			}
@@ -31,9 +26,9 @@ function init() {
 	}
 
 	scene.add(cube);
-	cube.rotation.y = Math.PI/4;
-	cube.rotation.x = Math.PI/4;
-	
+	cube.rotation.y = Math.PI / 4;
+	cube.rotation.x = Math.PI / 4;
+
 	//Apply settings to renderer and create canvas
 	renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.setSize(window.innerWidth, window.innerHeight);
@@ -43,36 +38,115 @@ function init() {
 
 init();
 
-let turned = false;
+let turning = true;
+let counter = 0;
+let speed = 0.01;
 
-//i == M
-//j == E
-//k == S
+let oldCube = [
+	{ j: 1, k: -1 },
+	{ j: 1, k: 0 },
+	{ j: 1, k: 1 },
+	{ j: 0, k: 1 },
+	{ j: -1, k: 1 },
+	{ j: -1, k: 0 },
+	{ j: -1, k: -1 },
+	{ j: 0, k: -1 },
+];
 
 function animation(time) {
-	//Apply all the changes to the scene and then render
-	// cube.rotation.z += 0.02;
-	// cube.rotation.y += 0.02;
-	// cube.rotation.x += 0.02;
-	
-	if (!turned) {
-		let cubiePositions = [];
+	// cube.rotation.z += 0.01;
+	cube.rotation.y += 0.01;
+	// cube.rotation.x += 0.01;
+	if (turning) {
+		//L
 		for (let child of cube.children) {
 			if (child.i == -1) {
-				child.applyMatrix4( new THREE.Matrix4().makeRotationX(Math.PI/2) );
+				child.applyMatrix4(new THREE.Matrix4().makeRotationX(Math.PI / 2));
 			}
-			cubiePositions.push({i:child.i, j:child.j})
-			// if (child.i == 0) {
-			// 	child.applyMatrix4( new THREE.Matrix4().makeRotationX(Math.PI) );	
-			// }
-			// if (child.j == 0) {
-			// 	child.applyMatrix4( new THREE.Matrix4().makeRotationY(Math.PI) );
-			// }
 		}
-		
-		turned = true;
-	} 
+		for (let child of cube.children) {
+			if (child.i == -1) {
+				if (child.j==0^child.k==0) {
+					if (child.j==0) {
+						child.j = child.k;
+						child.k = 0;
+					} else {
+						child.k = -child.j;
+						child.j = 0;
+					}
+				} if (child.j==0&&child.k==0) {
+					continue;
+				} else {
+					if (child.j!=child.k) {
+						child.k *= -1;
+					} else {
+						child.j *= -1;
+					}
+				}
+			}
+		}
+		// //B
+		// for (let child of cube.children) {
+		// 	if (child.k == -1) {
+		// 		child.applyMatrix4(new THREE.Matrix4().makeRotationZ(Math.PI / 2));
+		// 	}
+		// }
+		//F'
+		for (let child of cube.children) {
+			if (child.k == 1) {
+				child.applyMatrix4(new THREE.Matrix4().makeRotationZ(Math.PI / 2));
+			}
+		}
+
+		//needs tuning
+		for (let child of cube.children) {
+			if (child.k == 1) {
+				if (child.j==0^child.i==0) {
+					if (child.j==0) {
+						child.j = child.i;
+						child.i = 0;
+					} else {
+						child.i = -child.j;
+						child.j = 0;
+					}
+				} if (child.j==0&&child.i==0) {
+					continue;
+				} else {
+					if (child.j!=child.i) {
+						child.i *= -1;
+					} else {
+						child.j *= -1;
+					}
+				}
+			}
+		}
+
+		// // D
+		// for (let child of cube.children) {
+		// 	if (child.j == -1) {
+		// 		child.applyMatrix4(new THREE.Matrix4().makeRotationY(Math.PI / 2));
+		// 	}
+		// }
+
+		/////////////////////////////////////////////////////////////////////////
+
+		// for (let child of cube.children) {
+		// 	if (child.k == 1) {
+		// 		child.applyMatrix4(new THREE.Matrix4().makeRotationZ(Math.PI / 2));
+		// 	}
+		// }
+		turning = false;
+	}
 	renderer.render(scene, camera);
 }
 
-
+function arrayRotate(arr, reverse) {
+	if (reverse) {
+		arr.unshift(arr.pop());
+		arr.unshift(arr.pop());
+	} else {
+		arr.push(arr.shift());
+		arr.push(arr.shift());
+	}
+	return arr;
+}
