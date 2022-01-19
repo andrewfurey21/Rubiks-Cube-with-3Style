0873049e0930e468ef70210c8378c9f658d2bb37
andrewfurey21 <andrewfurey2003@gmail.com>
2022-01-19 15:10:19 +0000

    figured out all the moves properly, beside wide moves and rotations

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ let cube;
 
 let camera, scene, renderer;
 
+let turning = true;
+let counter = 0;
+let speed = 0.01;
+
 function init() {
 	camera = new THREE.PerspectiveCamera(
 		70,
@@ -12,6 +16,7 @@ function init() {
 		0.01,
 		10
 	);
+
 	camera.position.z = 1;
 	scene = new THREE.Scene();
 	cube = new THREE.Group();
@@ -36,26 +41,44 @@ function init() {
 	document.body.appendChild(renderer.domElement);
 }
 
+function animation(time) {
+	cube.rotation.y += 0.005;
+
+	if (turning) {
+
+		// I(-1); //L
+		// I(0); //M
+		// I(1); //R'
+
+		// J(-1); //D
+		// J(0); //E
+		// J(1); //U'
+
+		// K(-1); // B
+		// K(0); // S'
+		// K(1); //F'
+
+		turning = false;
+	}
+	renderer.render(scene, camera);
+}
+
 init();
 
-let turning = true;
-let counter = 0;
-let speed = 0.01;
-
-function animation(time) {
-	// cube.rotation.z += 0.01;
-	cube.rotation.y += 0.005;
-	// cube.rotation.x += 0.01;
-	if (turning) {
-		//L
+//LMR
+function I(slice) {
+	if (slice > Math.floor(n / 2) || slice < Math.floor(-n / 2)) {
+		console.error(`There is no ${slice} slice here.`);
+	} else {
+		//R'
 		for (let child of cube.children) {
-			if (child.i == -1) {
+			if (child.i == slice) {
 				child.applyMatrix4(new THREE.Matrix4().makeRotationX(Math.PI / 2));
 			}
 		}
 
 		for (let child of cube.children) {
-			if (child.i == -1) {
+			if (child.i == slice) {
 				if ((child.j == 0) ^ (child.k == 0)) {
 					if (child.j == 0) {
 						child.j = -child.k;
@@ -75,45 +98,22 @@ function animation(time) {
 				}
 			}
 		}
+	}
+}
 
-		//B
+//UED
+function J(slice) {
+	if (slice > Math.floor(n / 2) || slice < Math.floor(-n / 2)) {
+		console.error(`There is no ${slice} slice here.`);
+	} else {
 		for (let child of cube.children) {
-			if (child.k == -1) {
-				child.applyMatrix4(new THREE.Matrix4().makeRotationZ(Math.PI / 2));
-			}
-		}
-
-		for (let child of cube.children) {
-			if (child.k == -1) {
-				if ((child.j == 0) ^ (child.i == 0)) {
-					if (child.j == 0) {
-						child.j = child.i;
-						child.i = 0;
-					} else {
-						child.i = -child.j;
-						child.j = 0;
-					}
-				} else if (child.j == 0 && child.i == 0) {
-					continue;
-				} else {
-					if (child.j != child.i) {
-						child.j *= -1;
-					} else {
-						child.i *= -1;
-					}
-				}
-			}
-		}
-
-		// D
-		for (let child of cube.children) {
-			if (child.j == -1) {
+			if (child.j == slice) {
 				child.applyMatrix4(new THREE.Matrix4().makeRotationY(Math.PI / 2));
 			}
 		}
 
 		for (let child of cube.children) {
-			if (child.j == -1) {
+			if (child.j == slice) {
 				if ((child.k == 0) ^ (child.i == 0)) {
 					if (child.k == 0) {
 						child.k = -child.i;
@@ -133,16 +133,40 @@ function animation(time) {
 				}
 			}
 		}
+	}
+}
 
-		// F'
+//BSF
+function K(slice) {
+	if (slice > Math.floor(n / 2) || slice < Math.floor(-n / 2)) {
+		console.error(`There is no ${slice} slice here.`);
+	} else {
 		for (let child of cube.children) {
-			if (child.k == 1) {
+			if (child.k == slice) {
 				child.applyMatrix4(new THREE.Matrix4().makeRotationZ(Math.PI / 2));
 			}
 		}
 
-		turning = false;
+		for (let child of cube.children) {
+			if (child.k == slice) {
+				if ((child.j == 0) ^ (child.i == 0)) {
+					if (child.j == 0) {
+						child.j = child.i;
+						child.i = 0;
+					} else {
+						child.i = -child.j;
+						child.j = 0;
+					}
+				} else if (child.j == 0 && child.i == 0) {
+					continue;
+				} else {
+					if (child.j != child.i) {
+						child.j *= -1;
+					} else {
+						child.i *= -1;
+					}
+				}
+			}
+		}
 	}
-	renderer.render(scene, camera);
 }
-
