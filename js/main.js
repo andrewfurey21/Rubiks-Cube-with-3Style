f065f5d4296dba5766605934cc76e0e592150f23
andrewfurey21 <andrewfurey2003@gmail.com>
2022-01-22 05:53:28 +0000

    added double turns

--- js/main.js
+++ js/main.js
@@ -5,13 +5,9 @@ let cube;
 
 let camera, scene, renderer;
 
-let turning = true;
 let counter = 0;
-let speed = 5; // in degrees
 
 let sequence;
-
-let currentAngle = 0;
 
 function init() {
 	camera = new THREE.PerspectiveCamera(
@@ -38,70 +34,101 @@ function init() {
 	cube.rotation.y = Math.PI / 4;
 	cube.rotation.x = Math.PI / 4;
 
-	//Apply settings to renderer and create canvas
 	renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.setAnimationLoop(animation);
 	document.body.appendChild(renderer.domElement);
 
 	sequence = scramble(20);
+	logScramble(sequence)
+	for (let i = 0; i < sequence.length; i += 2) {
+		applyTurn(sequence[i], sequence[i + 1]);
+	}
 }
 
+let turning = true;
+let appliedTurn = false;
+let currentMove = 0;
+
+let currentAngle = 0;
+let lastAngle = 0;
+let speed = 3; // in degrees
+
 function animation(time) {
-	cube.rotation.y += 0.005;
+	// cube.rotation.y += 0.005;
 
-	if (turning) {
-		// currentAngle += speed;
-
-		// I(-1); //L
-		// I(0); //M
-		// I(1, -1); //R
-
-		// J(-1, -1); //D'
-		// J(0); //E
-		// J(1); //U'
-
-		// K(-1, -1); // B'
-		// K(0); // S'
-		// K(1); // F'
-
-		turning = false;
-	}
 	renderer.render(scene, camera);
 }
 
 init();
 
+function applyTurn(turn, direction) {
+	let dir = scrambleDirection(direction).dir;
+	let turns = scrambleDirection(direction).turns;
+	switch (turn) {
+		case "R":
+			I(1, -dir, turns);
+			break;
+		case "M":
+			I(0, dir, turns);
+			break;
+		case "L":
+			I(-1, dir, turns);
+			break;
+		case "U":
+			J(1, -dir, turns);
+			break;
+		case "E":
+			J(0, dir, turns);
+			break;
+		case "D":
+			J(-1, dir, turns);
+			break;
+		case "F":
+			K(1, -dir, turns);
+			break;
+		case "S":
+			K(0, -dir, turns);
+			break;
+		case "B":
+			K(-1, dir, turns);
+			break;
+	}
+}
+
 //L M R'
-function I(slice, direction = 1) {
-	if (slice > Math.floor(n / 2) || slice < Math.floor(-n / 2)) {
+function I(slice, direction = 1, turns = 1) {
+	if (slice > Math.floor(n / 2) || slice < Math.floor(-n / 2) || turns < 0) {
 		console.error(`There is no ${slice} slice here.`);
 	} else {
 		for (let child of cube.children) {
 			if (child.i == slice) {
-				child.applyMatrix4(new THREE.Matrix4().makeRotationX(direction*Math.PI / 2));
+				child.applyMatrix4(
+					new THREE.Matrix4().makeRotationX((turns * direction * Math.PI) / 2)
+				);
 			}
 		}
-
-		for (let child of cube.children) {
-			if (child.i == slice) {
-				if ((child.j == 0) ^ (child.k == 0)) {
-					if (child.j == 0) {
-						child.j = -child.k*direction;
-						child.k = 0;
+		for (let i = 1; i <= turns; i++) {
+			for (let child of cube.children) {
+				if (child.i == slice) {
+					if ((child.j == 0) ^ (child.k == 0)) {
+						if (child.j == 0) {
+							child.j = -child.k * direction;
+							child.k = 0;
+						} else {
+							child.k = child.j * direction;
+							child.j = 0;
+						}
+					} else if (child.j == 0 && child.k == 0) {
+						continue;
 					} else {
-						child.k = child.j*direction;
-						child.j = 0;
-					}
-				} else if (child.j == 0 && child.k == 0) {
-					continue;
-				} else {
-					if (child.j != child.k) {
-						child.k *= -direction;
-						child.j *= direction;
-					} else {
-						child.j *= -direction;
-						child.k *= direction;
+						if (child.j != child.k) {
+							child.k *= -direction;
+							child.j *= direction;
+						} else {
+							child.j *= -direction;
+							child.k *= direction;
+						}
 					}
 				}
 			}
@@ -110,35 +137,39 @@ function I(slice, direction = 1) {
 }
 
 //U' E D
-function J(slice, direction=1) {
-	if (slice > Math.floor(n / 2) || slice < Math.floor(-n / 2)) {
-		console.error(`There is no ${slice} slice here.`);
+function J(slice, direction = 1, turns = 1) {
+	if (slice > Math.floor(n / 2) || slice < Math.floor(-n / 2) || turns < 0) {
+		console.error(`Error: Invalid input`);
 	} else {
 		for (let child of cube.children) {
 			if (child.j == slice) {
-				child.applyMatrix4(new THREE.Matrix4().makeRotationY(direction*Math.PI / 2));
+				child.applyMatrix4(
+					new THREE.Matrix4().makeRotationY((turns * direction * Math.PI) / 2)
+				);
 			}
 		}
 
-		for (let child of cube.children) {
-			if (child.j == slice) {
-				if ((child.k == 0) ^ (child.i == 0)) {
-					if (child.k == 0) {
-						child.k = -child.i*direction;
-						child.i = 0;
+		for (let i = 1; i <= turns; i++) {
+			for (let child of cube.children) {
+				if (child.j == slice) {
+					if ((child.k == 0) ^ (child.i == 0)) {
+						if (child.k == 0) {
+							child.k = -child.i * direction;
+							child.i = 0;
+						} else {
+							child.i = child.k * direction;
+							child.k = 0;
+						}
+					} else if (child.k == 0 && child.i == 0) {
+						continue;
 					} else {
-						child.i = child.k*direction;
-						child.k = 0;
-					}
-				} else if (child.k == 0 && child.i == 0) {
-					continue;
-				} else {
-					if (child.k != child.i) {
-						child.i *= -direction;
-						child.k *= direction;
-					} else {
-						child.k *= -direction;
-						child.i *= direction;
+						if (child.k != child.i) {
+							child.i *= -direction;
+							child.k *= direction;
+						} else {
+							child.k *= -direction;
+							child.i *= direction;
+						}
 					}
 				}
 			}
@@ -147,35 +178,38 @@ function J(slice, direction=1) {
 }
 
 //B S' F'
-function K(slice, direction=1) {
-	if (slice > Math.floor(n / 2) || slice < Math.floor(-n / 2)) {
-		console.error(`There is no ${slice} slice here.`);
+function K(slice, direction = 1, turns = 1) {
+	if (slice > Math.floor(n / 2) || slice < Math.floor(-n / 2) || turns < 0) {
+		console.error(`Error: Invalid ipnut`);
 	} else {
 		for (let child of cube.children) {
 			if (child.k == slice) {
-				child.applyMatrix4(new THREE.Matrix4().makeRotationZ(direction*Math.PI / 2));
+				child.applyMatrix4(
+					new THREE.Matrix4().makeRotationZ((turns * direction * Math.PI) / 2)
+				);
 			}
 		}
-
-		for (let child of cube.children) {
-			if (child.k == slice) {
-				if ((child.j == 0) ^ (child.i == 0)) {
-					if (child.j == 0) {
-						child.j = child.i*direction;
-						child.i = 0;
+		for (let i = 1; i <= turns; i++) {
+			for (let child of cube.children) {
+				if (child.k == slice) {
+					if ((child.j == 0) ^ (child.i == 0)) {
+						if (child.j == 0) {
+							child.j = child.i * direction;
+							child.i = 0;
+						} else {
+							child.i = -child.j * direction;
+							child.j = 0;
+						}
+					} else if (child.j == 0 && child.i == 0) {
+						continue;
 					} else {
-						child.i = -child.j*direction;
-						child.j = 0;
-					}
-				} else if (child.j == 0 && child.i == 0) {
-					continue;
-				} else {
-					if (child.j != child.i) {
-						child.j *= -direction;
-						child.i *= direction;
-					} else {
-						child.i *= -direction;
-						child.j *= direction;
+						if (child.j != child.i) {
+							child.j *= -direction;
+							child.i *= direction;
+						} else {
+							child.i *= -direction;
+							child.j *= direction;
+						}
 					}
 				}
 			}
--- js/scramble.js
+++ js/scramble.js
@@ -1,37 +1,53 @@
 //Can only use two sides per axis, scrambles will never include wide moves or slices
+
+let moves = [
+	["R", "L"],
+	["U", "D"],
+	["F", "B"],
+];
+
+let additions = [" ", "' ", "2 "];
+
 function scramble(length) {
-	let moves = [
-		["R", "L"],
-		["U", "D"],
-		["F", "B"],
-	];
-
-	let additions = [" ", "' ", "2 "];
-
-	let scramble = "";
+	let scramble = [];
 	let pastMoves = [];
 	let lastAxis;
-    let lastSide;
-    //lastSide is for later, to fix the scramble program for allowing two sides of the same axis to appear next to each other
-    //without making the scrambles redundant.
+	let lastSide;
+
 	for (let i = 0; i < length; i++) {
 		let axis = Math.floor(Math.random() * moves.length);
-        let side = Math.floor(Math.random() * moves[axis].length);
+		let side = Math.floor(Math.random() * moves[axis].length);
 
-        if (i > 0) {
+		if (i > 0) {
 			if (lastAxis != axis) {
-				scramble += moves[axis][side] + additions[Math.floor(Math.random() * additions.length)];
-				pastMoves.push(moves[side]);
-                lastSide = side;
+				scramble.push(moves[axis][side]);
+				scramble.push(additions[Math.floor(Math.random() * additions.length)]);
 			} else i--;
 		} else {
-			scramble += moves[axis][side] + additions[Math.floor(Math.random() * additions.length)];
-			pastMoves.push(moves[side]);
-            lastSide = side;
+			scramble.push(moves[axis][side]);
+			scramble.push(additions[Math.floor(Math.random() * additions.length)]);
 		}
 		lastAxis = axis;
-
 	}
 	return scramble;
+}
+
+function logScramble(scramble) {
+	let s = "";
+	for (let i = 0; i < scramble.length; i++) {
+		s += scramble[i];
+	}
+	console.log(s);
+	return s;
+}
+
+function scrambleDirection(direction) {
+	if (direction==additions[0]) {
+		return {dir:1, turns:1};
+	} else if (direction==additions[1]) {
+		return {dir:-1, turns:1};
+	} else if (direction==additions[2]) {
+		return {dir:1, turns:2};
+	}
 }
 
